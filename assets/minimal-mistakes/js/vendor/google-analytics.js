7fbefb51f7766845f1d403ed438039d18ab17b64
Guillermo Calvo <guillermocalvo@yahoo.com>
2015-07-26 15:03:01 +0200

    Change the way Google Analytics are included.

---
+++ assets/minimal-mistakes/js/vendor/google-analytics.js
@@ -0,0 +1,17 @@
+---
+# Asynchronous Google Analytics snippet
+---
+var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+var _gaq = _gaq || [];
+_gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+_gaq.push(['_setAccount', '{{ site.owner.google.analytics }}']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
