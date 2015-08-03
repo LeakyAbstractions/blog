de1212de4dda5ef4b3ca7cb871525dd3a06b6913
Guillermo Calvo <guillermocalvo@yahoo.com>
2015-08-04 01:58:12 +0200

    Add RSS feed widget to *curriculum vitae*.

--- assets/curriculum/main.js
+++ assets/curriculum/main.js
@@ -21,4 +21,16 @@ jQuery(document).ready(function($) {
 
     /* Bootstrap Tooltip for Skillset */
     $('.level-label').tooltip();
+
+    /* jQuery RSS - https://github.com/sdepold/jquery-rss */
+	var rss_feeds = $("#rss-feeds");
+    rss_feeds.rss(
+		rss_feeds.attr('feed-url'),
+        {
+			limit: rss_feeds.attr('feed-limit'),
+			effect: rss_feeds.attr('feed-effect'), /* show, slide, slideFast, slideSynced, slideFastSynced */
+			layoutTemplate: "<div class='item'>{entries}</div>",
+			entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>' + rss_feeds.attr('feed-more') + '</a></div>'
+        }
+    );
 });
