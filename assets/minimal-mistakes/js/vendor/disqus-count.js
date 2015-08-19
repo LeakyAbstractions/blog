625f0f35d7fec476a659e18e7e8bcb0ce3b4c65e
Guillermo Calvo <guillermocalvo@yahoo.com>
2015-08-20 01:31:27 +0200

    Add mechanism to show number of comments.

---
+++ assets/minimal-mistakes/js/vendor/disqus-count.js
@@ -0,0 +1,14 @@
+---
+# Disqus
+---
+
+var disqus_shortname = '{{ site.owner.disqus-shortname }}';
+
+/* DON'T EDIT BELOW THIS LINE */
+
+(function () {
+	var s = document.createElement('script'); s.async = true;
+	s.type = 'text/javascript';
+	s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+	(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
